1747e5556fd1a463a02f0e8fa1eb42966ad61dd6
carl1984r <carl.cc@gmail.com>
2019-08-30 16:27:58 -0500

    Updated routes, updated root page, login page, sign in page.  Removed navbar - utilized hamb menu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   root 'welcome#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
