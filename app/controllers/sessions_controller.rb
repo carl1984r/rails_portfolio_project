d75e92cbe4ee8f0763de6677eaf2d1f4ab9e198c
carl1984r <carl.cc@gmail.com>
2019-08-30 00:57:27 -0500

    Updated sessions controller with create, destroy and helper methods.  Updated session view (new) with login form.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,33 @@
 class SessionsController < ApplicationController
 
+  helper_method :logged_in?, :current_user
+  layout "static"
+
   def new
+
+  end
+
+  def create
+
+    user = User.find_by(email: params[:session][:name].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      #redirect_to user_plural_path(user)
+    else
+      flash[:login_error] = "Incorrect email/password.  Try again."
+      render :new
+    end
+
+  end
+
+  def destroy
+
+    if logged_in?
+      session.clear
+      redirect_to '/'
+    else
+      redirect_back(fallback_location: root_path)
+    end
 
   end
 
