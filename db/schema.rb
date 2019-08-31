637a9b7c33fce4259ebeb3ffb7f55ad4f3c61856
carl1984r <carl.cc@gmail.com>
2019-08-31 01:26:03 -0500

    Migrated Bizlists.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_173314) do
+ActiveRecord::Schema.define(version: 2019_08_31_060757) do
+
+  create_table "bizlists", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
