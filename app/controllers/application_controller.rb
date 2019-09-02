e995b48ac3fcef1bb09094208dff92d17eea706d
carl1984r <carl.cc@gmail.com>
2019-09-01 23:30:51 -0500

    Added flash message to signup, name validation, defined require_login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
     @user = User.find_by(id: session[:user_id])
   end
 
+  def require_login
+    unless logged_in?
+      flash[:login] = "Please login to view content."
+      redirect_to login_path
+    end
+  end
+
 end
