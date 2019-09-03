c89c85a485ace3ba5eea932b90041a138aa3bdff
carl1984r <carl.cc@gmail.com>
2019-09-03 04:10:59 -0500

    Updated errors messages per user. Adjusted error message view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :bizlists
 
-  validates :username, presence: { message: "cannot be empty." }
-  validates :email, presence: { message: "required." }
-  validates :email, uniqueness: { message: "already used. Try another email." }
+  validates :username, presence: { message: "cannot be empty" }
+  validates :email, presence: true, uniqueness: true
 
 end
