493b5fa4abfc81aa75303462fb3d37cf78613ed7
carl1984r <carl.cc@gmail.com>
2019-09-03 15:18:14 -0500

    Updated flash alert message for login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_bizlists_path(user)
     else
+      flash.now[:error]= "Valid username and password both required."
       render :new
     end
 
