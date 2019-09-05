3dc63d45087cf2a2dc94463aa78995eeb2247303
carl1984r <carl.cc@gmail.com>
2019-09-05 03:30:11 -0500

    Updated login + sign up styling and login flash message.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_bizlists_path(user)
     else
-      flash.now[:error] = "Valid username and password required."
+      flash.now[:error] = "Valid username and password required"
       render :new
     end
 
