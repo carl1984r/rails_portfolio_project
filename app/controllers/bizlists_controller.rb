c146bf971f921ac0be73127a2599baef01db4c9b
carl1984r <carl.cc@gmail.com>
2019-09-06 21:48:02 -0500

    Updated style for bizlist (index) + controller (index, new) + created link for creating new favorites list.

--- app/controllers/bizlists_controller.rb
+++ app/controllers/bizlists_controller.rb
@@ -1,6 +1,11 @@
 class BizlistsController < ApplicationController
+  before_action :require_login
 
   def index
+    @username = current_user.username
+  end
+
+  def new
     @username = current_user.username
   end
 
