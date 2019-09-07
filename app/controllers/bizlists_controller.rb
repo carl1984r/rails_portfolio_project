748a37af9c23b1e4ba6bda167acbee784870cae0
carl1984r <carl.cc@gmail.com>
2019-09-07 13:42:11 -0500

    Added box-shadow - (commented out for the option), added init_user_bizlist as a before_action - (commented out for the option), updated Welcome statement, added new.html.erb with user_nav template to be rendered.

--- app/controllers/bizlists_controller.rb
+++ app/controllers/bizlists_controller.rb
@@ -1,12 +1,33 @@
 class BizlistsController < ApplicationController
   before_action :require_login
+  before_action :init_user
+  before_action :init_username
+  #before_action :init_user_bizlist
 
   def index
-    @username = current_user.username
+    @bizlists = current_user.bizlists
   end
 
   def new
+    @bizlist = Bizlist.new
+  end
+
+  private
+
+  def init_user
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def init_username
     @username = current_user.username
   end
+
+  #def init_user_bizlist
+    #@bizlist = Bizlist.find_by(id: params[:id])
+    #unless @bizlist.user == current_user
+      #flash[:danger] = "Some message"
+      #redirect_to bizlists_path
+    #end
+  #end
 
 end
