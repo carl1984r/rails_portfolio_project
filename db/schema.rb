487923a642b0eb13e8feb3ffea2fb2dbf16c5f85
carl1984r <carl.cc@gmail.com>
2019-09-12 02:41:46 -0500

    Removed rating column from business. Ran rake:db migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_072418) do
+ActiveRecord::Schema.define(version: 2019_09_12_073830) do
 
   create_table "bizlists", force: :cascade do |t|
     t.string "name"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2019_09_12_072418) do
     t.string "name"
     t.string "address"
     t.text "description"
-    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
