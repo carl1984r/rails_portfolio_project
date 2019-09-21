1e339ddad31ec7bfa1d4b2e27070f7580cd2c38a
carl1984r <carl.cc@gmail.com>
2019-09-21 08:51:33 -0500

    Added route if no route matches.

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '*path', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
