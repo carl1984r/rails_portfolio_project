5765ba54f8427766c6cc2f374f757ba84e38bd1f
carl1984r <carl.cc@gmail.com>
2019-09-25 13:02:54 -0500

    Added scss per displaying rating stars, updated reviews controller to include username - updated Flash[:review_error] message, updated business show page to display review count, updated text area per reviews/_form page.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,11 +11,12 @@ class ReviewsController < ApplicationController
   def create
     if !!params[:review] && !!params[:review][:experience] && !!params[:review][:location] && !!params[:review][:value]
       @review = Review.new(review_params)
+      @review.user = @username
       @business.reviews << @review
       flash[:review_created] = "Review successfully created"
       redirect_to business_path(@business)
     else
-      flash[:review_error] = "Review form must be completed"
+      flash[:review_error] = "Review form must be completed. Comments are optional."
       redirect_to new_business_review_path(@business)
     end
   end
@@ -23,7 +24,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit( :experience, :location, :value )
+    params.require(:review).permit( :experience, :location, :value, :comment )
   end
 
   def init_business
