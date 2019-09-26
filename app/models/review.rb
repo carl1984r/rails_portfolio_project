cec9cd5b8c4d905e0c25fe1b389e62e3d23095d8
carl1984r <carl.cc@gmail.com>
2019-09-26 04:40:07 -0500

    Added formula to calculate average and round no nearest 10th within models/review, updated business/show view to include average.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   has_many :business_reviews
   has_many :businesses, through: :business_reviews
+
+  def review_average(var1, var2, var3)
+    (((var1 + var2 + var3)/3.to_f)*10).ceil/10.0
+  end
+
 end
