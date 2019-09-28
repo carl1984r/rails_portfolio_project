153ce987ca98310b0482280bbd0bc56810784bf6
carl1984r <carl.cc@gmail.com>
2019-09-28 02:37:47 -0500

    Added validates name + address, validates uniqueness of address to business model, cleaned up businesses/_form, cleaned up common/_user_nav.

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,4 +2,7 @@ class Business < ApplicationRecord
   belongs_to :bizlist, optional: true
   has_many :business_reviews
   has_many :reviews, through: :business_reviews
+
+  validates :name, presence: true
+  validates :address, presence: true, uniqueness: true
 end
